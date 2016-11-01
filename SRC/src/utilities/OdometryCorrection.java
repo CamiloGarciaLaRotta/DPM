10a298d3dc0b1169f63bc43bc1c67c9080972584
DestructiveReasoning <harleyw@hotmail.com>
2016-11-01 14:48:11 +0000

    Implemented Odometry Correction

--- SRC/src/utilities/OdometryCorrection.java
+++ SRC/src/utilities/OdometryCorrection.java
@@ -4,7 +4,7 @@
 package utilities;
 
 import chassis.Main;
-import chassis.LightIntensitySensor;
+import utilities.LightIntensitySensor;
 
 /**
  * @version 0.1
@@ -21,7 +21,6 @@ public class OdometryCorrection extends Thread {
 	private double lastPosition[];
 	private float lastIntensity;
 	private final boolean useCorrection = true;
-	private final boolean updateAll[] = {true, true, true};
 
 	/**
 	 * 
@@ -31,7 +30,7 @@ public class OdometryCorrection extends Thread {
 		this.odometer = odometer;
 		count = 0;
 		lastPosition = new double[3]; //x, y, theta
-		odometer.getPosition(lastPosition, updateAll);	//need to set theta - may as well get current x and y
+		odometer.getPosition(lastPosition, Util.UPDATE_ALL);	//need to set theta - may as well get current x and y
 	}
 
 	// run method (required for Thread)
@@ -55,28 +54,16 @@ public class OdometryCorrection extends Thread {
 			
 			if(intensity < LightIntensitySensor.LINE_DETECTED) //Detected black
 			{
-				System.out.println("         LINE");
 				double currentPosition[] = new double[3];
-				odometer.getPosition(currentPosition, updateAll);
-				//check theta (see if we changed heading and turned)
-				if(Math.abs(currentPosition[2] - lastPosition[2]) < THRESHOLD_THETA)
-				{
-					if(Math.abs(Util.SQUARE_LENGTH + lastPosition[1] - currentPosition[1]) < THRESHOLD_POS ||	//no line missed
-							Math.abs(Util.SQUARE_LENGTH + lastPosition[0] - currentPosition[0]) < THRESHOLD_POS)
-					{
-						if(Math.abs(currentPosition[2] - 0.00) < THRESHOLD_THETA) //+x direction
-							odometer.setX(lastPosition[0] + Util.SQUARE_LENGTH);
-						else if(Math.abs(currentPosition[2] - (-Math.PI/2)) < THRESHOLD_THETA) //-y
-							odometer.setY(lastPosition[1] - Util.SQUARE_LENGTH);
-						else if(Math.abs(currentPosition[2] - (-Math.PI)) < THRESHOLD_THETA) //-x
-							odometer.setX(lastPosition[0] - Util.SQUARE_LENGTH);
-						else if(Math.abs(currentPosition[2] - (-Math.PI*3/2)) < THRESHOLD_THETA) //+y
-							odometer.setY(lastPosition[1] + Util.SQUARE_LENGTH);
-					}
-				}
-				//if nothing was done in the above block we either missed a line or turned and we just need to update
-				lastPosition = currentPosition.clone();
-				count++;
+				odometer.getPosition(currentPosition, Util.UPDATE_ALL);
+				
+				//check if vertical line...
+				double distanceX = (currentPosition[0]/Util.SQUARE_LENGTH) - Math.floor(currentPosition[0]/Util.SQUARE_LENGTH);
+				if(distanceX < Util.GRIDLINE_THRESHOLD) odometer.setX(Math.floor(currentPosition[0]/Util.SQUARE_LENGTH)*Util.SQUARE_LENGTH);
+				
+				//check if horizontal line...
+				double distanceY = (currentPosition[1]/Util.SQUARE_LENGTH) - Math.floor(currentPosition[1]/Util.SQUARE_LENGTH);
+				if(distanceY < Util.GRIDLINE_THRESHOLD) odometer.setY(Math.floor(currentPosition[1]/Util.SQUARE_LENGTH)*Util.SQUARE_LENGTH);
 			}
 			
 			// this ensure the odometry correction occurs only once every period
