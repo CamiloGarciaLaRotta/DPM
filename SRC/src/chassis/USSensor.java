2c5d5114e4a2e292530c3d8fb822889a25a2e20b
Juliette Regimbal <JRegimbal@users.noreply.github.com>
2016-11-01 13:47:51 -0400

    Add mutex to USSensor

--- SRC/src/chassis/USSensor.java
+++ SRC/src/chassis/USSensor.java
@@ -13,14 +13,19 @@ public class USSensor {
 	private SampleProvider usValue;
 	public static final float FIELD_BOUNDS = 120;
 	
+	private Object mutex;	//to avoid race conditions
+	
 	public USSensor(Port usPort) {
 		this.usSensor = new EV3UltrasonicSensor(usPort);
 		this.usValue = usSensor.getMode("Distance");
 		this.usData = new float[usValue.sampleSize()];
+		this.mutex = new Object();
 	}
 	
 	public float getFilteredDataBasic() {
-		usSensor.fetchSample(usData, 0); //Store distance in usData
+		synchronized(mutex) {
+			usSensor.fetchSample(usData, 0); //Store distance in usData
+		}
 		return (usData[0] * 100.0f >= FIELD_BOUNDS) ? FIELD_BOUNDS : usData[0] * 100.0f; //Cap data at NO_WALL, scale data by 100.
 	}
 	
