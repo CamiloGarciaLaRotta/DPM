47d7793ab7b32eba45dc74c2ce648147f39ad068
DestructiveReasoning <harleyw@hotmail.com>
2016-11-01 16:45:32 +0000

    Added some constants to Util

--- SRC/src/utilities/Util.java
+++ SRC/src/utilities/Util.java
@@ -9,11 +9,14 @@ public class Util {
 	
 	// robot
 	public static LCDInfo lcd;
-	public static final long ODOMETER_PERIOD = 25; //Hz
-	public static final double WHEEL_RADIUS = 2.025; //cm
-	public static final double TRACK = 15.01; //cm 
-	public static final double US_TO_CENTER = 4.50; //cm from us sensor to center of wheels
-	public static final double INTENSITY_TO_CENTER = 12; //cm from ligh sensor to center of wheels
+	public static final long ODOMETER_PERIOD		= 25; //Hz
+	public static final double WHEEL_RADIUS			= 2.025; //cm
+	public static final double TRACK				= 15.01; //cm 
+	public static final double US_TO_CENTER			= 4.50; //cm from us sensor to center of wheels
+	public static final double INTENSITY_TO_CENTER	= 12; //cm from ligh sensor to center of wheels
+	public static final double GRIDLINE_THRESHOLD	= 0.1;
+
+	public static final boolean[] UPDATE_ALL = new boolean[] {true,true,true};
 
 	// special coordinates
 	public static final double [] GOAL_ZONE = {80, 80}; //TODO not actual value, to be defined
