f11329426918af9fe95bfe192d55636afd572117
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2016-11-06 17:51:29 -0500

    main search algorithm complete

--- SRC/src/utilities/Avoider.java
+++ SRC/src/utilities/Avoider.java
@@ -1,6 +1,10 @@
 package utilities;
 
-import chassis.Main;
+import java.awt.Rectangle;
+
+import chassis.USSensor;
+import utilities.Odometer.LINEDIR;
+import utilities.Search.SearchState;
 
 /**
  * @version 0.1
@@ -9,28 +13,102 @@ import chassis.Main;
  */
 public class Avoider extends Thread{
 	
-	public static boolean safe;
 	private Odometer odo;
 	private Navigation nav;
+	private USSensor usSensor;
 	
-	private static final int AVOID_SPEED = 60;
+	private double[] currPos = new double[3];
+	private double distance;
 	
-	//TODO complete and test
-	// Search requires this thread to change its state once its safe to continue
+	private Rectangle RED;
+	private Rectangle currRect = new Rectangle();
 	
-	public Avoider(Odometer odo) {
+	// corners
+	private final Rectangle x1 = new Rectangle(-5,-5,10, 10);
+	private final Rectangle x2 = new Rectangle(300,-5,10, 10);
+	private final Rectangle x3 = new Rectangle(300,300,10, 10);
+	private final Rectangle x4 = new Rectangle(-5,300,10, 10);
+	
+	public enum AvoidState {Disabled, Enabled};
+	public static AvoidState avoidState = AvoidState.Disabled;
+	
+	//TODO TODO TODO TODO
+	// - Search requires this thread to change its state once its safe to continue
+	// - Handle CW and CCW avoidance depending on the position of the robot on the map
+	// - Right now avoidance only avoids blocks or zones one at a time, make dynamic
+	// - Discuss with Software if use of java.awt.rectangle is pertinent
+	// - Is it alright to hardcode corner values? they are absolute and final regardless anything
+	
+	public Avoider(Odometer odo, Navigation nav, USSensor usSensor) {
 		this.odo = odo;
+		this.nav = nav;
+		this.usSensor = usSensor;
+		int red_width = 1;	// to be updated via constructor 
+		int red_height = 2;	// depending on wifi RED coordinates
+		this.RED = new Rectangle(0*40,9*40,red_width*40, red_height*40);
 	}
 
 	@Override
 	public void run() {
-		while(Main.state != Main.RobotState.Avoiding) {
-			if(Thread.interrupted()) return;
-			try { Thread.sleep(300); } catch(Exception ex) {ex.printStackTrace();}
+		while(true){
+			
+			distance = usSensor.getMedianSample(Util.US_SAMPLES);
+			
+			// build current position rectangle
+			odo.getPosition(this.currPos);
+			currRect.setBounds((int)(currPos[0]*Util.SQUARE_LENGTH), (int)(currPos[1]*Util.SQUARE_LENGTH),5, 5);
+			
+			boolean CCW = chooseOrientation(currPos);
+			
+			if(Avoider.avoidState == AvoidState.Enabled) {			
+				
+				// check for physical obstacles
+				if(distance < Util.MIN_D){
+					odo.stopMotors();
+					
+					do{
+						linearAvoidance(CCW);
+					} while (usSensor.getMedianSample(Util.US_SAMPLES) < Util.MIN_D);
+					
+					// no more obstacle ahead, advance a bit before returning control to Search
+					odo.moveCM(LINEDIR.Forward, Util.WOOD_MIN_WIDTH, true);
+				}
+				
+				// check for RED zone
+				if(RED.contains(currRect)){
+					do{
+						linearAvoidance(CCW);
+					} while (RED.contains(currRect));
+				}
+				
+				// check for corners zones
+				if(x1.contains(currRect) || x2.contains(currRect) ||
+					x3.contains(currRect) || x4.contains(currRect)){
+					// return to GREEN, nothing to do in a corner
+					Search.searchState = SearchState.Default;
+				}
+			}
+			
+			// lower stress on CPU
+			try {
+				Thread.sleep(Util.AVOIDER_PERIOD);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		this.safe = false;
-		
 	}
-	
+
+	// choose rotation sense depending on current position and heading
+	private boolean chooseOrientation(double[] currPos) {
+		//TODO
+		return false;
+	}
+
+	// linear set of instructions to avoid obstacle
+	private void linearAvoidance(boolean CCW) {
+		int coeff = (CCW) ? -1 : 1;
+		nav.turnBy(coeff*90);
+		odo.moveCM(LINEDIR.Forward, Util.WOOD_MIN_WIDTH, true);
+		nav.turnBy(-1*coeff*90);
+	}
 }
--- SRC/src/utilities/Util.java
+++ SRC/src/utilities/Util.java
@@ -5,25 +5,33 @@ import chassis.LCDInfo;
 public class Util {
 	// field
 	public static final double SQUARE_LENGTH = 30.48; //cm
-	public static final double FIELD_BOUNDARY = 3.0 * SQUARE_LENGTH; 
+	public static final double FIELD_BOUNDARY = 3.0 * SQUARE_LENGTH;
+	public static final double GRIDLINE_THRESHOLD	= 0.1;	//Error margin (in cm) to consider position at a gridline
+	public static final double ZONE_THRESHOLD		= 1.0;	//Error margin (in cm) for position at the green/red zone
+	
+	// field objects
+	public static final double FOAM_HEIGHT			= 4.9;	//Height of the styrofoam blocks
+	public static final double WOOD_MIN_WIDTH		= 15;	//minimum distance to travel to avoid wooden block
+	
+	// avoider
+	public static final double MIN_D 				= 15; // minimum distance in cm to object to avoid		
 	
 	// robot
 	public static LCDInfo lcd;
-	public static final long ODOMETER_PERIOD		= 25; 	//Hz
 	public static final double WHEEL_RADIUS			= 2.18; //cm
 	public static final double TRACK				= 16.6; //cm 
 	public static final double US_TO_CENTER			= 4.50; //cm from us sensor to center of wheels
 	public static final double INTENSITY_TO_CENTER	= 12; 	//cm from ligh sensor to center of wheels
-	public static final double GRIDLINE_THRESHOLD	= 0.1;	//Error margin (in cm) to consider position at a gridline
-	public static final double ZONE_THRESHOLD		= 1.0;	//Error margin (in cm) for position at the green/red zone
-	public static final double BLOCK_HEIGHT			= 4.9;	//Height of the styrofoam blocks
+	public static final long ODOMETER_PERIOD		= 25; 	//Hz
+	public static final int AVOIDER_PERIOD			= 1000;  //ms to wait between each avoidance check;
 	public static final int US_SAMPLES				= 6; 	//Default amount of samples to take for median filter
 	public static final double FORKLIFT_HEIGHT		= 15.0;	//Length of forklift 
-
+	
 	public static final boolean[] UPDATE_ALL = new boolean[] {true,true,true};
 
 	// special coordinates
 	public static final double [] GOAL_ZONE = {80, 80}; //TODO not actual value, to be defined
+
 	public static void setLCD(LCDInfo lcd) {
 		Util.lcd = lcd;
 	}
