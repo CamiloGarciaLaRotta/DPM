a94989c19753edf71f0e44ef362a22c9b5fb862f
DestructiveReasoning <harleyw@hotmail.com>
2016-11-15 18:57:30 +0000

    Fixed drop points

--- SRC/src/chassis/Forklift.java
+++ SRC/src/chassis/Forklift.java
@@ -32,8 +32,8 @@ public class Forklift {
 
 	public void liftToTower(int towerHeight) { //Move lift to height defined by the current tower height
 		double height = (towerHeight == 0) ? 0.0 : (double) towerHeight * Util.FOAM_HEIGHT + Util.DROP_SPACE;
-		int theta = (int)((height/Util.FORKLIFT_ROPE_RADIUS) * 180.0 / Math.PI);
-		forkliftMotor.rotateTo(theta);
+		int theta = (int)(((Util.FORKLIFT_HEIGHT - height)/Util.FORKLIFT_ROPE_RADIUS) * 180.0 / Math.PI);
+		forkliftMotor.rotateTo(-theta);
 	}
 	
 	public void toggleGrip() {
